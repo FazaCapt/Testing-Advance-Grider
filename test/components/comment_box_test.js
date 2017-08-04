e2a91e6d57c060d79a895fabd6053d6cf65cb995
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-04 08:41:17 +0700

    Simulating Events

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -19,4 +19,20 @@ describe('CommentBox', () => {
     it('it has a button', () => {
         expect(component.find('button')).to.exist;
     });
+
+    describe('entering some text', () => {
+        beforeEach(() => {
+            component.find('textarea').simulate('change', 'new comment');
+        });
+
+        it('shows that text in the text area', () => {
+
+        });
+    
+        it('when submitted, clears the input', () => {
+
+        });
+    })
+
+    
 }); 
