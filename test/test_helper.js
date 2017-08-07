47c6d0d12b4739e36830b6c9a529a408ded1854d
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-07 08:55:10 +0700

    More JSDom Setup

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,12 +1,22 @@
 import jsdom from 'jsdom';
-
+import jquery from 'jquery';
 
 // Set up testing environtment to run like a browser in the command line
 // window ----> global
 global.document = jsdom.jsdom('<!doctype html><html><body></body>')
 global.window = global.document.defaultView;
+const $ = jquery(global.window);
+
+
+
 // build 'renderComponent' helper that should render a given react class
 
+
+
 // build helper for simulting events
+
+
+
+
 
 // set up chai-jquery
